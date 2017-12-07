d1252397b9fd4414f40f6748aeaacd5863fc21b8
Shawn Garner <shawndgarner@gmail.com>
2017-12-07 11:39:45 +0000

    adding addition game states

--- src/main/scala/net/nomadicalien/connect4/GameState.scala
+++ src/main/scala/net/nomadicalien/connect4/GameState.scala
@@ -8,6 +8,30 @@ case class InitialState(
                        playField: PlayField
                        ) extends GameState
 
+case class EnterPlayerOneState(
+                         playField: PlayField
+                       ) extends GameState
+
+case class EnterPlayerTwoState(
+                         playField: PlayField,
+                         player1: Player
+                       ) extends GameState
+
+case class InPlayState(
+                         playField: PlayField,
+                         player1: Player,
+                         player2: Player,
+                         turn: Player
+                       ) extends GameState
+
+case class GameOverState(
+                           playField: PlayField,
+                           player1: Player,
+                           player2: Player,
+                           winner: Player
+                         ) extends GameState
+
+
 object GameState {
   lazy val empty = InitialState(PlayField.empty)
 }

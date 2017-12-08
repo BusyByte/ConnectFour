b4cb2fc8dddc42e146a1a4f80918d020a288bcb7
Shawn Garner <shawn.garner@banno.com>
2017-12-08 14:52:44 -0600

    updated main method for game loop, missing implicits soon to be fixed

--- src/main/scala/net/nomadicalien/connect4/Main.scala
+++ src/main/scala/net/nomadicalien/connect4/Main.scala
@@ -2,4 +2,23 @@ package net.nomadicalien.connect4
 
 class Main extends App {
 
+  def gameLoop(gameState: GameState): Unit = {
+    import Writer.Implicits._
+    println(implicitly[Writer[GameState]].write(gameState))
+
+    import Instructor.Implicits._
+    println(implicitly[Instructor[GameState]].instruct(gameState))
+
+    val input = Console.in.readLine()
+
+    import StateTransition.Implicits._
+    val updatedState = implicitly[StateTransition[GameState]].transition(input, gameState)
+    updatedState match {
+      case ExitGameState => ()
+      case _ => gameLoop(gameState)
+    }
+
+  }
+
+  gameLoop(GameState.empty)
 }

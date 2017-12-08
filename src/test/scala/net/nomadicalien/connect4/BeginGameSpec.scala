242328319adfa88c3d13e54f3f1131a311397ed2
Shawn Garner <shawndgarner@gmail.com>
2017-12-08 13:11:23 +0000

    implemented BeginGameSpec

--- src/test/scala/net/nomadicalien/connect4/BeginGameSpec.scala
+++ src/test/scala/net/nomadicalien/connect4/BeginGameSpec.scala
@@ -1,6 +1,7 @@
 package net.nomadicalien.connect4
 
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 
 /*
 
@@ -17,5 +18,47 @@ And player 1 is in control of the playfield
 
 */
 class BeginGameSpec extends Specification {
+  "EnterPlayerTwoState" should {
 
+    "transition states" in new context {
+      newState must_=== expectedState
+    }
+
+    "playfield has correct number of rows and columns" in new context {
+      newState must beLike {
+        case InPlayState(playField, _, _, _) =>
+          playField.length must_=== PlayField.numColumns
+          playField(0).length must_=== PlayField.numRows
+      }
+    }
+
+    "player1 is red" in new context {
+      newState must beLike {
+        case InPlayState(_, Player1(_, _, Red), _, _) => ok
+      }
+    }
+
+    "player2 is black" in new context {
+      newState must beLike {
+        case InPlayState(_, _, Player2(_, _, Black), _) => ok
+      }
+    }
+
+    "player1 in control" in new context {
+      newState must beLike {
+        case InPlayState(_, p1, _, control) => p1 must_== control
+      }
+    }
+  }
+
+  trait context extends Scope {
+    import StateTransition.Implicits._
+    lazy val playField = PlayField.empty
+    lazy val player1 = Player1(new NonEmptyString("player1"))
+    lazy val state = EnterPlayerTwoState(playField, player1)
+    lazy val player2Name = "player2"
+    lazy val player2 = Player2(new NonEmptyString(player2Name))
+    lazy val newState = implicitly[StateTransition[EnterPlayerTwoState]].transition(player2Name, state)
+    lazy val expectedState = InPlayState(playField, player1, player2, player1)
+  }
 }

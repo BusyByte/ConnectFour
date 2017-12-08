12f733cab63e9434041609817a0ba518129dcf32
Shawn Garner <shawn.garner@banno.com>
2017-12-08 13:53:24 -0600

    implement VerticalWinSpec

--- src/test/scala/net/nomadicalien/connect4/VerticalWinSpec.scala
+++ src/test/scala/net/nomadicalien/connect4/VerticalWinSpec.scala
@@ -1,6 +1,7 @@
 package net.nomadicalien.connect4
 
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 
 /*
 
@@ -17,5 +18,63 @@ And vice versa
 
 */
 class VerticalWinSpec extends Specification {
+  "player 1" should {
+    "win vertically" in new context {
+      val picks = List(
+        0, 1,
+        0, 2,
+        0, 3,
+        0
+      )
 
+      val newState = performPicks(playerOneInControlPlayState, picks)
+      newState must beLike {
+        case g @ GameOverState(_, _, _, winner) if winner.number == 1 =>
+          val prompt = gameOverInstructor.instruct(g).split(lineSeparator).toList
+          prompt must_== List(Instructions.gameOverMessage, "Player 1 has won")
+      }
+
+
+
+    }
+  }
+
+  "player 2" should {
+    "win vertically" in new context {
+      val picks = List(
+        1, 0,
+        2, 0,
+        3, 0,
+        1, 0
+      )
+
+      val newState = performPicks(playerOneInControlPlayState, picks)
+      newState must beLike {
+        case g @ GameOverState(_, _, _, winner) if winner.number == 2 =>
+          val prompt = gameOverInstructor.instruct(g).split(lineSeparator).toList
+          prompt must_== List(Instructions.gameOverMessage, "Player 2 has won")
+      }
+
+    }
+  }
+
+
+  trait context extends Scope with TestData {
+
+    def performPicks(state: InPlayState, picks: List[Int]): GameState = {
+      picks.foldLeft[GameState](state) { (state, pick) =>
+        state match {
+          case i: InPlayState =>
+            stateTransition.transition(pick.toString, i)
+          case _ => state
+        }
+      }
+    }
+
+    import StateTransition.Implicits._
+    lazy val stateTransition = implicitly[StateTransition[InPlayState]]
+
+    import Instructor.Implicits._
+    lazy val gameOverInstructor = implicitly[Instructor[GameOverState]]
+  }
 }
